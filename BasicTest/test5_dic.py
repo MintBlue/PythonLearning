217f4fbf3c9c7c9e55cdb2afc88faed78d0a3041
mint <1143776192@qq.com>
2019-04-26 00:31:11 +0800

    字典

--- BasicTest/test5_dic.py
+++ BasicTest/test5_dic.py
@@ -1,8 +1,15 @@
 # [] 列表
 # () 元组 不可变
 # {} 字典
-a = {1: 'a', 2: 'b', 3: 'c'}
+a = {1: 'a', 2: 'b', 3: 'c', 4: 'a'}
 print(a)
 print(a[1])
 print(a.items())
 print(len(a))
+print(a.keys())
+print(1 in a.keys())
+print(range(5))
+print(range(0, 5))
+print(list(range(0, 5)))
+print(a.values())
+print('a' in set(a.values()))
